771562723252080f616a7e68b60754b7757e3b3e
rvmalikrm <rm.rvmalik@gmail.com>
2024-10-24 15:02:41 +0530

    Initial commit2

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import time
+from selenium.webdriver.chrome import webdriver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+class TestCCTBRepublish:
+    def test_RepublishCCTB(self):
+        driver=webdriver.Chrome()
+        driver.maximize_window()
+        driver.implicitly_wait(5)
+        driver.get("https://cctbweb-qa.azurewebsites.net/umbraco")
+        driver.find_element(By.NAME,"username").send_keys("ravi.malik@gus.global")
+        driver.find_element(By.NAME, "password").send_keys("Ravi@12345")
+        driver.find_element(By.XPATH,"//button[@class='btn umb-button__button btn-success umb-button--m']").click()
+        time.sleep(4)
+        driver.get("https://cctbweb-qa.azurewebsites.net/umbraco/dialogs/republish.aspx?xml=true")
+        wait=WebDriverWait(driver, 3)
+        wait.until(expected_conditions.presence_of_element_located((By.XPATH,"//input[@id='body_bt_go']")))
+        driver.find_element(By.XPATH,"//input[@id='body_bt_go']").click()
+        time.sleep(5)
+        publish_content_upto_date=driver.find_element(By.XPATH,"//input[@value='Ok']").get_attribute("value")
+        assert publish_content_upto_date =="Ok"
+        print("The website cache has been refreshed. All publish content is now up to date. While all unpublished content is still unpublished")
+        driver.quit()
